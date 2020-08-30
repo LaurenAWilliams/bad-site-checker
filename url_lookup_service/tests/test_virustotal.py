9b45b1698b8995029c7df79e0596c1bf06f1c397
Lauren Williams <laurenwilliamssoftwareengineer@gmail.com>
2020-08-30 17:59:41 +0100

    ULS-4: Utilities for Virus Total API with unit tests

---
+++ url_lookup_service/tests/test_virustotal.py
@@ -0,0 +1,41 @@
+from url_lookup_service.utils.virustotal import post_url_scan
+from url_lookup_service.utils.virustotal import get_url_scan_report
+from unittest.mock import patch, MagicMock
+
+
+@patch('urllib.request.urlopen')
+def test_post_url_scan(mock_urlopen):
+    mock_resp_read = b"""
+{"scan_id": "cf4b367e49bf0b22041c6f065f4aa19f3cfe39c8d5abc0617343d1a66c6a26f5-1598802494"}
+    """
+
+    mm = MagicMock()
+    mm.getcode.return_value = 200
+    mm.read.return_value = mock_resp_read
+    mock_urlopen.return_value = mm
+
+    expected = \
+        "cf4b367e49bf0b22041c6f065f4aa19f3cfe39c8d5abc0617343d1a66c6a26f5" \
+        "-1598802494", 200
+    actual = post_url_scan("google.com")
+    assert expected == actual
+
+
+@patch('urllib.request.urlopen')
+def test_get_url_scan_report(mock_urlopen):
+    mock_scan_id = "cf4b367e49bf0b22041c6f065f4aa19f3cfe39c8d5abc0617343d1a66c6a26f5-1598802494"
+    mock_report = b"""
+{"positives": 0, "scans": {"Feodo Tracker": {"detected": false, "result": "clean site"}}}
+    """
+
+    mm = MagicMock()
+    mm.getcode.return_value = 200
+    mm.read.return_value = mock_report
+    mock_urlopen.return_value = mm
+
+    expected = {
+                   "safe": True,
+                   "details": {},
+               }, 200
+    actual = get_url_scan_report(mock_scan_id)
+    assert expected == actual
---
+++ url_lookup_service/utils/virustotal.py
@@ -0,0 +1,69 @@
+"""
+Utility to provide an interface for lookup up URLs on VirusTotal.
+This is currently implemented using the free tier, so we're limited
+to 4 lookups per minute.
+"""
+
+import os
+import json
+from dotenv import load_dotenv
+from urllib import request, parse
+
+load_dotenv()
+
+VT_API_KEY = os.getenv("VT_API_KEY")
+
+
+def post_url_scan(query_string):
+    """
+
+    :param query_string: url we want to lookup
+    :return: scan_id, status_code: scan id returned by virus total so we can
+             lookup the scan later
+    """
+    url = "https://www.virustotal.com/vtapi/v2/url/scan"
+
+    data_dict = {"apikey": VT_API_KEY, "url": query_string}
+    data = parse.urlencode(data_dict).encode()
+    req = request.Request(url=url, data=data)
+    resp = request.urlopen(req)
+
+    if resp.getcode() == 200:
+        data = resp.read()
+        decoded_data = json.loads(data)
+        return decoded_data['scan_id'], 200
+    else:
+        return None, resp.getcode()
+
+
+def get_url_scan_report(scan_id):
+    """
+
+    :param scan_id: scan id of previous scan we submitted
+    :return: dict, status_code: success or failure response, with reasoning
+    """
+    url = "https://www.virustotal.com/vtapi/v2/url/report" \
+          "?apikey=%s&resource=%s"
+    query_url = url % (VT_API_KEY, scan_id)
+    resp = request.urlopen(query_url)
+    failed_scans = dict()
+
+    if resp.getcode() == 200:
+        data = resp.read()
+        decoded_data = json.loads(data)
+        if decoded_data['positives'] != 0:
+            safe = False
+            scans = decoded_data['scans']
+            for scan in scans:
+                if scans[scan]['detected']:
+                    failed_scans[scan] = scans[scan]['result']
+        else:
+            safe = True
+
+        return {
+                   "safe": safe,
+                   "details": failed_scans,
+               }, 200
+
+    else:
+        return None, resp.getcode()
