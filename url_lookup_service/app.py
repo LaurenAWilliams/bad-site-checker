1b714daaf49517fd731108f30e511999b24b3b13
Lauren Williams <laurenwilliamssoftwareengineer@gmail.com>
2020-08-31 13:17:32 +0100

    ULS-7: Basic app to lookup urls and check if they're malicious or not. Refinements to come.

--- url_lookup_service/app.py
+++ url_lookup_service/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from utils.virustotal import get_url_scan_report, post_url_scan
 
 app = Flask(__name__)
 
@@ -8,10 +9,26 @@ def root():
     return jsonify({"text": "Hello World!"}), 200
 
 
-@app.route('/urlinfo/1/<hostname_port>/<path_query>', methods=['GET'])
-def url_lookup(hostname_port, path_query):
-    return jsonify({"hostname_port": hostname_port,
-                    "path_query": path_query})
+@app.route('/urlinfo/1/<path:route>', methods=['GET'])
+def url_lookup(route):
+
+    def _reconstruct_url():
+        """Add proper query back in since flask strips it"""
+        if request.query_string:
+            fixed_url = route + "?" + request.query_string.decode('utf-8')
+            return fixed_url
+        else:
+            return route
+
+    url = _reconstruct_url()
+    scan_id, _ = post_url_scan(url)
+    report, status_code = get_url_scan_report(scan_id)
+
+    return jsonify({
+        "lookup_url": url,
+        "safe": report['safe'],
+        "details": report['details'],
+    })
 
 
 if __name__ == '__main__':
--- url_lookup_service/tests/test_app.py
+++ url_lookup_service/tests/test_app.py
@@ -1,8 +1,28 @@
 import json
+from unittest.mock import patch, MagicMock
 
 
 def test_root(client):
     response = client.get('/')
     assert response.status_code == 200
     expected_response = {"text": "Hello World!"}
+    assert expected_response == json.loads(response.get_data())
+
+@patch('url_lookup_service.utils.virustotal')
+def test_url_lookup(mock_virustotal, client):
+    response = client.get('/urlinfo/1/auctionbowling.com')
+    mm = MagicMock()
+    mm.post_url_scan.return_value = "22", 200
+    mm.get_url_scan_report.return_value = {"safe": False, "details": {
+        "AutoShun": "malicious site"}}, 200
+    mock_virustotal.return_value = mm
+
+    assert response.status_code == 200
+    expected_response = {
+        "details": {
+            "AutoShun": "malicious site"
+        },
+        "lookup_url": "auctionbowling.com",
+        "safe": False
+    }
     assert expected_response == json.loads(response.get_data())
